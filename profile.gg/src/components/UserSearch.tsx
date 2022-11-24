dfbc9672f9cf76afd044f8d896c7cc1029991b1a
Pedro Delavechia Risso <37818660+PedroDelaRisso@users.noreply.github.com>
2022-11-24 14:32:59 +0000

    Readiciona unofficial valorant api

--- profile.gg/src/components/UserSearch.tsx
+++ profile.gg/src/components/UserSearch.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import ValorantAPI, { AccountFetchOptions, APIResponse } from 'unofficial-valorant-api';
 import { Button,
     Row,
     Col,
@@ -6,20 +7,47 @@ import { Button,
     Form,
     InputGroup,
 } from 'react-bootstrap';
+import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+const api = new ValorantAPI();
 
 async function getUserInfo(username: string, setLoading: Function, setLabelBotao: Function) {
     if (username.length > 0 && username.includes('#')) {
         const name = username.split('#')[0];
         const tag = username.split('#')[1];
-        /* PARTE DA BUSCA DE JOGADOR */
+        const accountFetchOptions: AccountFetchOptions = {
+            name,
+            tag,
+        }
+        console.log(accountFetchOptions);
+        setLoading(true);
+        setLabelBotao('...');
+        api.getAccount(accountFetchOptions)
+            .then((getAccountResponse) => {
+                if (getAccountResponse.error) {
+                    (getAccountResponse.error as any).forEach((error: any = {}) => {
+                        console.log(error.message);
+                        setLoading(false);
+                        setLabelBotao('Generate');
+                    });
+                }
+                if (getAccountResponse.data) {
+                    api.getMatchesByPUUID({ puuid: (getAccountResponse.data as any).puuid, region: 'br', size: 10 })
+                        .then((getMatchesResponse) => {
+                            console.log(getMatchesResponse.data);
+                            setLoading(false);
+                            setLabelBotao('Generate');
+                        });
+                }
+            });
     } else {
         console.log('Digita um trem válido por favor :/');
     }
 }
 
 function UserSearch() {
-    const [username, setUsername] = useState('LOUD Coreano#LLL'); // debug apenas, remover username depois
+    const [username, setUsername] = useState('LOUD Coreano#LLL'); // debug apenas, remover depois
     const [loading, setLoading] = useState(false);
     const [labelBotao, setLabelBotao] = useState('Generate');
     return (
