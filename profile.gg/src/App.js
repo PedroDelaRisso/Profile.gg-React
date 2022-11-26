cb5e07e5f40fc6a795527abe08a7a4a6c7a4c824
Pedro Delavechia Risso <p.delavechia.r@gmail.com>
2022-11-26 15:33:36 -0300

    Implementa React Router

---
+++ profile.gg/src/App.js
@@ -0,0 +1,64 @@
+import { useState } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Kills from "./components/Kills";
+import GameTime from "./components/GameTime";
+import Rank from "./components/Rank";
+import WinRate from "./components/WinRate";
+import BestMap from "./components/BestMap";
+import HitLocation from "./components/HitLocation";
+import SideBar from "./components/SideBar";
+import UserSearch from "./components/UserSearch";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+function App() {
+  const [userData, setUserData] = useState({});
+  return (
+    <>
+      <Router>
+        <div className="App">
+          <Routes>
+            <Route path="/" element={
+              <UserSearch setUserData={setUserData} />
+            }>
+            </Route>
+            <Route path="/profile" element={
+              <div className="vh-100 vw-100 d-flex flex-row bg-primary">
+                <SideBar playerName="SantaCleiton"></SideBar>
+                <div
+                  className="vh-100 d-flex flex-column justify-content-around app"
+                  style={{ flex: 3 }}
+                >
+                  <div
+                    className="d-flex flex-row justify-content-around flex-wrap"
+                    style={{ flex: 1 }}
+                  >
+                    <Kills abatesTotais={652}></Kills>
+                    <GameTime horas={13} minutos={45}></GameTime>
+                  </div>
+                  <div
+                    className="d-flex flex-row justify-content-around flex-wrap"
+                    style={{ flex: 1 }}
+                  >
+                    <Rank rankName="IMORTAL 3"></Rank>
+                    <WinRate winrate={51}></WinRate>
+                  </div>
+                  <div
+                    className="d-flex flex-row justify-content-around flex-wrap"
+                    style={{ flex: 1 }}
+                  >
+                    <BestMap mapName={"BIND"}></BestMap>
+                    <HitLocation head={21} body={55} leg={28}></HitLocation>
+                  </div>
+                </div>
+              </div>
+            }>
+            </Route>
+          </Routes>
+        </div>
+      </Router>
+    </>
+  );
+}
+
+export default App;
