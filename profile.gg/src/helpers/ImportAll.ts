a3efd96f92eebcb78b92ed721b1e9801c1748391
Pedro Delavechia Risso <37818660+PedroDelaRisso@users.noreply.github.com>
2022-11-29 19:17:10 +0000

    Define tipo de parâmetros no ImportAll

--- profile.gg/src/helpers/ImportAll.ts
+++ profile.gg/src/helpers/ImportAll.ts
@@ -1,6 +1,6 @@
 export default function importAll(r) {
     let images = {};
-    r.keys().forEach((item, index) => {
+    r.keys().forEach((item: any, index: string | number) => {
         images[item.replace('./', '')] = r(item);
     });
     return images;
